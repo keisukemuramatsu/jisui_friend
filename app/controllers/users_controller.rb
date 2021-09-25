daccb3f6830e7fa4173320913e19cb3524810487
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-09-25 14:59:09 +0000

    [Add]ユーザー情報編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
   def show
   end
   def index
-end
+  end
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+     render :edit
+    else
+     redirect_to users_path, notice: "ほかのユーザー情報は編集できません。"
+    end
   end
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path, notice: "編集しました。"
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
