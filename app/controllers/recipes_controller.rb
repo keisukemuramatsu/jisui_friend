f159dba8bfd2e0de1b5bd1992cce8bd44343ba34
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-09-25 14:25:05 +0000

    [Add]レシピ編集

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,10 +6,20 @@ class RecipesController < ApplicationController
     @recipes = Recipe.page(params[:page]).reverse_order
   end
   def show
+    @recipe = Recipe.find(params[:id])
   end
   def edit
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user == current_user
+     render :edit
+    else
+     redirect_to recipes_path, notice: "ほかのユーザーのレシピは編集できません。"
+    end
   end
   def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe.id), notice: "編集しました。"
   end
   def create
     @recipe = Recipe.new(recipe_params)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_083155) do
+ActiveRecord::Schema.define(version: 2021_09_25_134031) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_083155) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
