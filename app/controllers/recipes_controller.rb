a23759320889dbc4e4db333dab4ef9e7a1f3929b
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-09-29 15:46:54 +0000

    [Fix]スペースでタグ付け

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
   end
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe_tags = @recipe.tags
     @recipe_comment = RecipeComment.new
   end
   def edit
@@ -19,17 +20,22 @@ class RecipesController < ApplicationController
   end
   def update
     @recipe = Recipe.find(params[:id])
+    tag_list = params[:recipe][:tag_names].split(",")
+    @recipe.tags_save(tag_list)
     @recipe.update(recipe_params)
     redirect_to recipe_path(@recipe.id), notice: "編集しました。"
   end
   def create
     @recipe = Recipe.new(recipe_params)
+    tag_list = params[:recipe][:tag_names].split(nil)
+    @recipe.tags_save(tag_list)
     @recipe.user_id = current_user.id
     @recipe.save
     redirect_to recipes_path, notice: "登録しました."
   end
   def delete
   end
+  
   private
   def recipe_params
     params.require(:recipe).permit(:title, :image, :foodstuff, :instructions, :impression, :user_id)
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,21 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
+  has_many :recipe_tags
+  has_many :tags, through: :recipe_tags
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
   attachment :image
+  def tags_save(tag_list)
+    if self.tags != nil
+      recipe_tags_records = RecipeTag.where(recipe_id: self.id)
+      recipe_tags_records.destroy_all
+    end
+    tag_list.each do |tag|
+      inspected_tag = Tag.where(tag_name: tag).first_or_create
+      self.tags << inspected_tag
+    end
+
+  end
 end
