ca3330fc9be71fd9a4338017f8ca9d3a33b2f981
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-10-07 03:28:49 +0000

    [Fix]メール送信エラー

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from:     "自炊フレンド事務局",
-          cc:       "harukasoranoyouni@gmail.com"
+  default from:     "自炊フレンド<harukasoranoyouni@gmail.com>"
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+   :registrations => 'users/registrations',
+  }
   root to: "homes#top"
   resources :recipes do
     resource :favorites, only: [:create, :destroy]
     resources :recipe_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update], :controllers => {
-   :registrations => 'users/registrations',
-  }
+  resources :users, only: [:index, :show, :edit, :update]
 end
